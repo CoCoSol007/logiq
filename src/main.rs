50ccddcf189b867d784d97e80342aeda8f882ef9
CoCoSol007 <solois.corentin@gmail.com>
2025-09-18 23:11:13 +0200

    Add tiny CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+//! Main entry point for the logik DSL.
+
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let Some(path) = args.get(1) else {
+        println!("You must provid a file path: logik <File Path>");
+        return;
+    };
+
+    let file_content_result = fs::read_to_string(path);
+
+    if let Err(error) = file_content_result {
+        println!("Error: {}", error);
+        return;
+    }
+
+    let _file_content = file_content_result.unwrap();
 }
